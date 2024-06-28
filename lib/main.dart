ffff583fc2a115b75b4da4a870bd61321c560272
voquangtri <voquangtri2708@gmail.com>
2024-06-28 18:38:25 +0700

    upload all interfaces

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,148 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: const HomePage(),
+//       theme: ThemeData(
+//         useMaterial3: true,
+//       ),
+//     );
+//   }
+// }
+//
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key});
+//
+//   @override
+//   HomePageState createState() => HomePageState();
+// }
+//
+// class HomePageState extends State<HomePage> {
+//   int _selectedIndex = 0;
+//
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Trang chủ'),
+//         leading: const Icon(Icons.menu),
+//       ),
+//       body: Column(
+//         children: [
+//           Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: TextField(
+//               decoration: InputDecoration(
+//                 hintText: 'Tìm kiếm',
+//                 prefixIcon: const Icon(Icons.search),
+//                 border: OutlineInputBorder(
+//                   borderRadius: BorderRadius.circular(10),
+//                 ),
+//               ),
+//             ),
+//           ),
+//           const Padding(
+//             padding: EdgeInsets.all(8.0),
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 ChoiceChip(label: Text('Tất cả'), selected: false),
+//                 ChoiceChip(label: Text('Trắc nghiệm'), selected: true),
+//                 ChoiceChip(label: Text('Thẻ ghi nhớ'), selected: false),
+//               ],
+//             ),
+//           ),
+//           Expanded(
+//             child: GridView.builder(
+//               padding: const EdgeInsets.all(8.0),
+//               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//                 crossAxisCount: 2,
+//                 childAspectRatio: 3/2,
+//                 mainAxisSpacing: 10,
+//                 crossAxisSpacing: 10,
+//               ),
+//               itemCount: 6,
+//               itemBuilder: (context, index) {
+//                 return Card(
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Expanded(
+//                         // child: Image.network('https://via.placeholder.com/150', fit: BoxFit.cover),
+//                         child: Image.asset('asset/images/img1.jpg', fit: BoxFit.cover)
+//
+//                       ),
+//                       const Padding(padding: EdgeInsets.all(8.0),child: Text('Ôn tập CS 403'),                      ),
+//                       const Padding(padding: EdgeInsets.symmetric(horizontal: 8.0),child: Text('30 Thẻ'),
+//                       ),
+//                     ],
+//                   ),
+//                 );
+//               },
+//
+//
+//             ),
+//           ),
+//         ],
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Trang chủ',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.search),
+//             label: 'Tìm kiếm',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.brightness_6),
+//             label: 'Dark Mode',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.settings),
+//             label: 'Cài đặt',
+//           ),
+//         ],
+//         currentIndex: _selectedIndex,
+//         onTap: _onItemTapped,
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:quiz_make/screens/welcome/welcome_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const HomePage(),
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Trang chủ'),
-        leading: const Icon(Icons.menu),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Tìm kiếm',
-                prefixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ChoiceChip(label: Text('Tất cả'), selected: false),
-                ChoiceChip(label: Text('Trắc nghiệm'), selected: true),
-                ChoiceChip(label: Text('Thẻ ghi nhớ'), selected: false),
-              ],
-            ),
-          ),
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(8.0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 3/2,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-              ),
-              itemCount: 6,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        // child: Image.network('https://via.placeholder.com/150', fit: BoxFit.cover),
-                        child: Image.asset('asset/images/img1.jpg', fit: BoxFit.cover)
-
-                      ),
-                      const Padding(padding: EdgeInsets.all(8.0),child: Text('Ôn tập CS 403'),                      ),
-                      const Padding(padding: EdgeInsets.symmetric(horizontal: 8.0),child: Text('30 Thẻ'),
-                      ),
-                    ],
-                  ),
-                );
-              },
-
-
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Trang chủ',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Tìm kiếm',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.brightness_6),
-            label: 'Dark Mode',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Cài đặt',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
+    return GetMaterialApp(
+      title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: WelcomeScreen(),
     );
   }
 }
---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:quiz_make/constants.dart';
+import 'package:quiz_make/screens/quiz/quiz_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(flex: 2), //2/6
+                  Text(
+                    "Let's Play Quiz,",
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Enter your information below"),
+                  Spacer(), // 1/6
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "Full Name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                    ),
+                  ),
+                  Spacer(), // 1/6
+                  InkWell(
+                    onTap: () => Get.to(QuizScreen()),
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(kDefaultPadding * 0.75),
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Text(
+                        "Let's Start Quiz",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Spacer(flex: 2), // it will take 2/6 spaces
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
